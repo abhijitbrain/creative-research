66116827890828aef322745c1225cbc4b113aac5
abhijitbrain <abhijitbrain@gmail.com>
2017-08-20 18:20:52 +0530

    Add files via upload

---
+++ _1mp3.ino
@@ -0,0 +1,20 @@
+#include "SD.h"
+#define SD_ChipSelectPin 4
+#include "TMRpcm.h"
+#include "SPI.h"
+
+TMRpcm tmrpcm;
+
+void setup(){
+tmrpcm.speakerPin = 9;
+Serial.begin(9600);
+if (!SD.begin(SD_ChipSelectPin)) {
+Serial.println("SD fail");
+return;
+}
+
+tmrpcm.setVolume(5);
+tmrpcm.play("3.wav");
+}
+
+void loop(){  }
